a31d9fc1c1072d1a8c2b604e80005bc58b70170a
Start Coding Student 2 <session2@START-Coding-MBP-C02PP742G8WN.local>
2015-10-09 16:55:01 -0700

    I put the chart in the JS file.And added to the html

--- scripts.js
+++ scripts.js
@@ -1,3 +1,33 @@
 /**
  * Created by session2 on 10/9/15.
  */
+google.load('visualization', '1', {packages: ['corechart', 'bar']});
+google.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Rappers', 'Net Worth',],
+        [' Dr Dre', 810000000],
+        ['P Diddy', 730000000],
+        ['Jay-Z', 650000000],
+        ['Russell Simons', 340000000],
+        ['Master P', 250000000]
+    ]);
+
+    var options = {
+        title: 'Population of Largest U.S. Cities',
+        chartArea: {width: '50%'},
+        hAxis: {
+            title: 'Net Worth in millions',
+            minValue: 0
+        },
+        vAxis: {
+            title: 'Rappers'
+        }
+    };
+
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+}
